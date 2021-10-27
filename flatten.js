ab1bb506e71c0ca540f5148c0e5d155e1a2f9153
Kevin Mason <krmason2020@gmail.com>
2021-10-27 02:17:21 +0000

    completed flatten.js

---
+++ flatten.js
@@ -0,0 +1,32 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`💚💚💚Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🔥🔥🔥Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const flatten = (arr) => {
+  const resultArray = [];
+  arr.forEach((el) => {
+    if (Array.isArray(el)) {
+      //all good if we are only 2d
+      el.forEach((e) => resultArray.push(e));
+    } else {
+      resultArray.push(el);
+    }
+  });
+  return resultArray;
+};
+const res = flatten([1, 2, [3, 4], 5, [6]]);
+
+assertArraysEqual(res, [1, 2, 3, 4, 5, 6]);
