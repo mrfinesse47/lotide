21166398a1bb1aa36680dc1936d16259b11aad82
Kevin Mason <krmason2020@gmail.com>
2021-10-27 00:29:40 +0000

    completed assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`💚💚💚Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🔥🔥🔥Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); //passed
+// assertArraysEqual([2, 3, 4, 5], [3, 4, 5, 6]); //failed
