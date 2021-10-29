80b306cef422c0c95f9bed3eafa428e1fcd8de5a
Kevin Mason <krmason2020@gmail.com>
2021-10-29 00:02:42 +0000

    completed findKey.js

---
+++ findKey.js
@@ -0,0 +1,60 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔥🔥🔥Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKey = (obj, callback) => {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 4 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 4
+  ),
+  "Akaleri"
+);
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 4 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars > 3
+  ),
+  "Akaleri"
+);
