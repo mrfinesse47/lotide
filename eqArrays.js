37a8f5f0293f2146a2ea0d1726009bf42ceb627e
Kevin Mason <krmason2020@gmail.com>
2021-10-30 06:24:27 +0000

    completed deep array compare

--- eqArrays.js
+++ eqArrays.js
@@ -7,22 +7,55 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    //bug fix for arrays of different length
-    return false;
-  }
+  // console.log("arr:1", arr1, "arr:2", arr2);
+  // if (arr1.length !== arr2.length) {
+  //   //bug fix for arrays of different length
+  //   return false;
+  // }
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      //if both arrays are indeed arrays
+      if (arr1[i].length !== arr2[i].length) {
+        return false;
+      }
+      eqArrays(arr1[i], arr2[i]);
+    } else {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
   }
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false]
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false]
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [
+      [2, 3],
+      [4, 5],
+    ]
+  ),
+  false
+); // => should PASS
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [
+      [2, 3],
+      [4, 5],
+    ]
+  ),
+  false
+); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
--- eqObjects.js
+++ eqObjects.js
@@ -18,8 +18,6 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
   const obj1Keys = Object.keys(object1);
 
