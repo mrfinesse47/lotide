46f5d9640d25d3ef91a482a2a7a09ad92292a2cd
Kevin Mason <krmason2020@gmail.com>
2021-11-01 00:26:36 +0000

    eqArray bug fix, i was doing noting with the recursive function return, fixed it

--- eqArrays.js
+++ eqArrays.js
@@ -7,20 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-  //console.log("arr:1", arr1, "arr:2", arr2);
-  // if (arr1.length !== arr2.length) {
-  //   //bug fix for arrays of different length
-  //   return false;
-  // }
-
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
       //if both arrays are indeed arrays
-      if (arr1[i].length !== arr2[i].length) {
-        //console.log("bad length:arr1", arr1[i], "arr2:", arr2[i]);
+
+      if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
-      eqArrays(arr1[i], arr2[i]);
     } else {
       if (arr1[i] !== arr2[i]) {
         return false;
@@ -59,7 +55,28 @@ assertEqual(
   ),
   false
 ); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+assertEqual(eqArrays([[2, 3], [4], 2], [[2, 3], [4], 2]), true); // => true
+assertEqual(
+  eqArrays(
+    [
+      [2, 3],
+      [[4, 5], 6],
+    ],
+    [
+      [2, 3],
+      [[4, 5], 6],
+    ]
+  ),
+  true
+); // => false
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4][(2, [2, 3], [4], 3)], [4], 2],
+    [[2, 3], [4][(2, [2, 3], [4], 3)], [4], 2]
+  ),
+  true
+); // => true
 assertEqual(
   eqArrays(
     [
--- eqObjects.js
+++ eqObjects.js
@@ -43,6 +43,23 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
+const eqObjects = function (object1, object2) {
+  if (object1 === object2) {
+    return true;
+  }
+
+  let key1 = Object.keys(object1);
+  let key2 = Object.keys(object2);
+
+  if (key1.length != key2.length) return false;
+
+  for (const key of key1)
+    if (!key2.includes(key) || !eqObjects(object1[key], object2[key]))
+      return false;
+
+  return true;
+};
+
 //use with assert equal
 
 const ab = { a: "1", b: "2" };
