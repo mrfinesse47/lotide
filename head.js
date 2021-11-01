88e9e9ee9d561bce5163688fbab2055b31da9394
Kevin Mason <krmason2020@gmail.com>
2021-11-01 16:28:45 +0000

    exported module from asserEqual, imported to head.js

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚💚Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🔥🔥🔥Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = (arr) => {
   return arr[0];
