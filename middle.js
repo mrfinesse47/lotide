5eb53962f8ed92ceeb078b86fdfbcf7ff30b9102
Kevin Mason <krmason2020@gmail.com>
2021-11-01 20:32:36 +0000

    updated middle to remove its tests and moved to a new file in tests named middleTest

--- middle.js
+++ middle.js
@@ -1,28 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`💚💚💚Assertion Passed: [${actual}] === [${expected}]`);
-//   } else {
-//     console.log(`🔥🔥🔥Assertion Failed: [${actual}] !== [${expected}]`);
-//   }
-// };
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`💚💚💚Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`🔥🔥🔥Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
-};
-
-const eqArrays = (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const middle = (arr) => {
   const resultArr = [];
   if (arr.length > 2) {
@@ -36,15 +11,4 @@ const middle = (arr) => {
   return resultArr;
 };
 
-assertArraysEqual(middle([1]), []);
-//middle([1]); // => []
-assertArraysEqual(middle([1, 2]), []);
-// middle([1, 2]); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]);
-// middle([1, 2, 3]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// middle([1, 2, 3, 4, 5]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// middle([1, 2, 3, 4]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+module.exports = middle;
